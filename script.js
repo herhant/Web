c429dd7f90a04cb216a0bc8d81b462ded24e9aa7
Her Zamora <herhant@gmail.com>
2024-09-29 03:27:35 -0300

    agregue animacion a inicio y mejore header

--- script.js
+++ script.js
@@ -22,3 +22,38 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+/* Animación inicio */
+
+window.addEventListener('load', () => {
+    animarElementos();
+});
+
+window.addEventListener('scroll', () => {
+    animarElementos();
+});
+
+function animarElementos() {
+    const img = document.querySelector('.animarimg');
+    const texto = document.querySelector('.animarp');
+
+    const imgPosition = img.getBoundingClientRect().top;
+    const textoPosition = texto.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight;
+
+    // Si la imagen está en el viewport, añadir la clase 'in-view'
+    if (imgPosition < screenPosition && imgPosition > 0) {
+        img.classList.add('in-view');
+    } else {
+        // Si la imagen ya no está visible, remover la clase 'in-view'
+        img.classList.remove('in-view');
+    }
+
+    // Si el párrafo está en el viewport, añadir la clase 'in-view'
+    if (textoPosition < screenPosition && textoPosition > 0) {
+        texto.classList.add('in-view');
+    } else {
+        // Si el párrafo ya no está visible, remover la clase 'in-view'
+        texto.classList.remove('in-view');
+    }
+}
